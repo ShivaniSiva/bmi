d973b1314ef1cc76b35e6fb1b3d36d9a98e56c15
Shivani Sivakumar <shivani@Shivanis-MacBook-Air.local>
2022-07-11 18:15:45 +0200

    I have Love calc,bmi upgrade and a Leap year challenge completed

--- index.js
+++ index.js
@@ -113,14 +113,63 @@ lifeInWeeks(); */
 
 
 //Love calc
-function lovecalc(){
+/* function lovecalc(){
 let name = prompt("What is your name?");
 let cruschName = prompt("What is your crusch name?");
 
 let random = Math.floor(Math.random() * 100)+ 1;
 console.log(random);
 
-alert(name + " " + cruschName + " " + "Love " + random+ "%");
+if(random > 70){
+    alert(name + " " + cruschName + " " + "Love " + random + "% " + "you love each other like Kange and Kange");
 }
+if(random > 30 && random <= 70){
+    alert(name + " " + cruschName + " " + "Love " + random+ "%");  
+}
+if(random <= 30){
+    alert(name + " " + cruschName + " " + "Love " + random + "% " + "You flow like oil and water");
+}
+}
+lovecalc(); */
 
-lovecalc();
+
+//bmi calc with if else 
+/* function bmi(weight,height){
+    let bmi = weight / Math.pow(height,2);
+    let interpretation = Math.round(bmi);
+    
+    if(interpretation < 18.5){
+        alert("Your BMI is " + interpretation + ", so you are underweight.");
+    }
+    else if(interpretation >= 18.5 && interpretation <= 24.9){
+        alert("Your BMI is " + interpretation + ", so you are normal.");
+    }
+   // else(interpretation >= 24.9)
+   else{
+        alert("Your BMI is " + interpretation + ", so you are overweight.");
+    }
+}
+bmi(21.8,1.33); */
+
+
+//Leap Year
+function leapYear(){
+    let year = prompt("Can you tell me a year so i can figure it out this a leap year or not a leap year.")
+    if(year % 4 === 0){
+        if(year % 100 === 0){
+            if(year % 400 === 0){
+                alert("Leap year");
+            }
+            else{
+                alert("Not a leap year");
+            }
+        }
+        else{
+            alert("Leap year");
+        }
+    }
+    else{
+        alert("Not a leap year");
+    }
+}
+leapYear();
